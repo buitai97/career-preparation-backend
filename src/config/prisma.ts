fe2b5a89f111faaafe337636ec2ce2aae40bb51d
Tai Bui <taibui97@outlook.com>
2026-02-25 14:26:27 -0800

    prisma config

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,7 +1,21 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from '../generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import fs from 'fs'
+import path from 'path'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
-export default prisma;
+const ca = fs.readFileSync(
+    path.join(__dirname, '../../certs/ca.pem')
+)
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+    ssl: {
+        ca,
+    },
+})
+
+const prisma = new PrismaClient({
+    adapter,
+})
+
+export default prisma
